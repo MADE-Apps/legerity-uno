4fe62711d43cabd2eed742553dc71518e0b262a9
James Croft <jamz_c@hotmail.co.uk>
2022-04-07 09:13:14 +0100

    Updated tests to use a single common TestPlatforms prop across all test classes

--- samples/UnoSampleAppTests/Tests/AppBarButtonTests.cs
+++ samples/UnoSampleAppTests/Tests/AppBarButtonTests.cs
@@ -1,13 +1,6 @@
 namespace UnoSampleAppTests.Tests
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
     using Legerity;
-    using Legerity.Android;
-    using Legerity.IOS;
-    using Legerity.Web;
-    using Legerity.Windows;
     using NUnit.Framework;
     using Pages;
 
@@ -17,42 +10,6 @@ namespace UnoSampleAppTests.Tests
         public AppBarButtonTests(AppManagerOptions options) : base(options)
         {
         }
-
-        static IEnumerable<AppManagerOptions> TestPlatformOptions => new List<AppManagerOptions>
-        {
-            new AndroidAppManagerOptions
-            {
-                AppId = AndroidApplication,
-                AppActivity = AndroidApplicationActivity,
-                DriverUri = "http://localhost:4723/wd/hub",
-                LaunchAppiumServer = false
-            },
-            new IOSAppManagerOptions
-            {
-                AppId = IOSApplication,
-                DeviceName = "iPhone SE (3rd generation) Simulator",
-                DeviceId = "56755E6F-741B-478F-BB1B-A48E05ACFE8A",
-                OSVersion = "15.4",
-                DriverUri = "http://192.168.86.172:4723/wd/hub",
-                LaunchAppiumServer = false
-            },
-            new WebAppManagerOptions(
-                WebAppDriverType.EdgeChromium,
-                Path.Combine(Environment.CurrentDirectory))
-            {
-                Maximize = true, Url = WasmApplication, ImplicitWait = TimeSpan.FromSeconds(10)
-            },
-            new WebAppManagerOptions(
-                WebAppDriverType.Chrome,
-                Path.Combine(Environment.CurrentDirectory))
-            {
-                Maximize = true, Url = WasmApplication, ImplicitWait = TimeSpan.FromSeconds(10)
-            },
-            new WindowsAppManagerOptions(WindowsApplication)
-            {
-                DriverUri = "http://127.0.0.1:4723", LaunchWinAppDriver = true, Maximize = true
-            }
-        };
 
         [Test]
         public void ShouldClickAppBarButtonByAutomationId()
